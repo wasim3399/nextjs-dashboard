b42fdb1d82938f7e5da4c8610ab96369e20005cb
Wasim Rashid <wasim.rashid@developers.studio>
2024-05-16 12:47:09 +0500

    navigation with clsx

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ For more information, see the [course curriculum](https://nextjs.org/learn) on t
 # tailwind
 # css modules
 
-
+# clsx library
--- app/page.tsx
+++ app/page.tsx
@@ -42,8 +42,8 @@ export default function Page() {
           />
           <Image
             src="/hero-mobile.png"
-            width={1000}
-            height={760}
+            width={560}
+            height={620}
             className="block md:hidden"
             alt="Screenshots of the dashboard project showing desktop version"
           />
